a62b497b603fa744f3ec5603119efbac54fa17e3
yuwen-c <yuwen.azulejos@gmail.com>
2021-03-28 11:34:51 +0800

    show excerpt of articles on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,45 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
+  {console.log("data",data)}
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    {
+      data.allMarkdownRemark.edges.map(node => {
+        return(
+          <div>
+            <h2>{node.node.frontmatter.title}</h2>
+            <p>{node.node.excerpt}</p>
+          </div>
+        )
+      })
+    }
+
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          excerpt
+          html
+          frontmatter {
+            date
+            description
+            title
+          }
+        }
+      }
+    }
+  }
+`
