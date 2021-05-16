5810fb15aeec123d469d41fb6222c8a72e4a7edc
yuwen-c <yuwen.azulejos@gmail.com>
2021-05-16 10:15:22 +0800

    install tachyons and import to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import tachyons from "tachyons"
 
 // make the blog link, (allow our link title clickable)
 // if apply the <Link> directly to title, it'll show the underline...a tag style, ugly
