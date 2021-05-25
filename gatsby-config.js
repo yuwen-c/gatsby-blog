18488a79d4f2c07ebc63f6b6cba99bd5817d9a85
yuwen-c <yuwen.azulejos@gmail.com>
2021-05-25 20:11:06 +0800

    edit description in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `yuwen-c`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `yuwen-c's blog`,
     author: `@yuwen-c`,
     siteUrl: `https://yuwen-c.netlify.app/`,
   },
